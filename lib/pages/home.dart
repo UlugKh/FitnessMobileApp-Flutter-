677f6a31304f5de548c5b04081f429c9115e3411
Ulugbek Xamidov <230035@newuzbekistanuniversity.uz>
2025-11-12 16:16:37 +0500

    Added a search bar with 2 icons and a Search Pancake hint message

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,58 +7,126 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Breakfast",
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
+      appBar: appBar(),
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          _searchField(),
+        ],
+      )
+    );
+  }
+
+  Container _searchField() {
+    return Container(
+          margin: EdgeInsets.only(top: 40, left: 20, right: 20),
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Color(0xff1D1617).withOpacity(0.11),
+                blurRadius: 40,
+                spreadRadius: 0,
+              ),
+            ],
+          ),
+          child: TextField(
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              hintText: 'Search Pancake',
+              hintStyle: TextStyle(
+                fontSize: 14,
+                color: Color(0xffDDDADA),
+              ),
+              contentPadding: EdgeInsets.all(15),
+              prefixIcon: Padding(
+               
+                padding: const EdgeInsets.all(12.0),
+                child: SvgPicture.asset(
+                  'assets/icons/Search.svg',
+                ),
+              ),
+              suffixIcon: Container(
+                width: 100,
+                child: IntrinsicHeight(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      VerticalDivider(
+                        color: Colors.black,
+                        indent: 10,
+                        endIndent: 10,
+                        thickness: 0.1,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: SvgPicture.asset('assets/icons/Filter.svg'),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15),
+                borderSide: BorderSide.none,
+                )
+              )
+            ),
+          );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: Text(
+        "Breakfast",
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      backgroundColor: Colors.white,
+      centerTitle: true,
+      elevation: 0,
+      leading: GestureDetector(
+        onTap: () {
+          // Action when the back arrow is tapped
+        },
+        child: Container(
+          alignment: Alignment.center,
+          margin: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: Color(0xffF7F8F8),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: SvgPicture.asset(
+            'assets/icons/Arrow - Left 2.svg',
+            width: 20,
+            height: 20,
           ),
         ),
-        backgroundColor: Colors.white,
-        centerTitle: true,
-        elevation: 0,
-        leading: GestureDetector(
+      ),
+      actions: [
+        GestureDetector(
           onTap: () {
-            // Action when the back arrow is tapped
+            // Action when the dots icon is tapped
           },
           child: Container(
-            alignment: Alignment.center,
-            margin: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: Color(0xffF7F8F8),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: SvgPicture.asset(
-              'assets/icons/Arrow - Left 2.svg',
-              width: 20,
-              height: 20,
-            ),
+          alignment: Alignment.center,
+          margin: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: Color(0xffF7F8F8),
+            borderRadius: BorderRadius.circular(10),
           ),
+          width: 37,
+          child: SvgPicture.asset(
+            'assets/icons/dots.svg',
+            width: 5,
+            height: 5,
+          ),
+                  ),
         ),
-        actions: [
-          GestureDetector(
-            onTap: () {
-              // Action when the dots icon is tapped
-            },
-            child: Container(
-            alignment: Alignment.center,
-            margin: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: Color(0xffF7F8F8),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            width: 37,
-            child: SvgPicture.asset(
-              'assets/icons/dots.svg',
-              width: 5,
-              height: 5,
-            ),
-                    ),
-          ),
-        ]
-      ),
+      ]
     );
   }
 }
